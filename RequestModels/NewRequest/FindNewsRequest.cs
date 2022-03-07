7c1429675286ad29b7303f6704c8415510ba3657
phat loc <locpromh@gmail.com>
2022-03-07 22:09:54 +0700

    validate all

--- RequestModels/NewRequest/FindNewsRequest.cs
+++ RequestModels/NewRequest/FindNewsRequest.cs
@@ -8,7 +8,7 @@ namespace AstroBackEnd.RequestModels.NewRequest
 {
     public class FindNewsRequest
     {
-        
+        public int? Id { get; set; }
         public string? Title { get; set; }
 
         public string? Description { get; set; }
