0a5e219c4ca56bc5b798c9b374a0ea1b9614d336
LinhMD <maidinhlinh967@gmail.com>
2022-03-20 20:53:44 +0700

    check aspect

--- Services/Implement/AspectService.cs
+++ Services/Implement/AspectService.cs
@@ -64,6 +64,8 @@ namespace AstroBackEnd.Services.Implement
         {
             try
             {
+                if (_work.Aspects.FindAtDBPaging(a => a.PlanetBaseId == request.PlanetBaseId && a.PlanetCompareId == request.PlanetCompareId && a.AngleType == request.AngleType, a => a.Id, out int total).Any())
+                    throw new ArgumentException("Aspect already exist");
                 Planet checkPlanetBase = _work.Planets.Get(request.PlanetBaseId);
                 Planet checkPlanetCompare = _work.Planets.Get(request.PlanetCompareId);
                 if (checkPlanetBase == null)
@@ -175,6 +177,8 @@ namespace AstroBackEnd.Services.Implement
         {
             try
             {
+                if (_work.Aspects.FindAtDBPaging(a => a.PlanetBaseId == request.PlanetBaseId && a.PlanetCompareId == request.PlanetCompareId && a.AngleType == request.AngleType, a => a.Id, out int total).Any())
+                    throw new ArgumentException("Aspect already exist");
                 Validation.ValidNumberThanZero(id, "Id must be than zero");
                 Planet checkPlanetBase = _work.Planets.Get(request.PlanetBaseId);
                 Planet checkPlanetCompare = _work.Planets.Get(request.PlanetCompareId);
